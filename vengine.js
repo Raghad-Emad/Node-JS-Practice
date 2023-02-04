e022facd2eaac1b158e46fba3e5ff800066ffd15
raghad-emad <raghademaad163@gmail.com>
2023-02-04 10:08:45 +0200

    View engine & ejs

---
+++ vengine.js
@@ -0,0 +1,33 @@
+const express = require('express');
+
+// expressx app 
+const app = express();
+
+// register view engine
+app.set('view engine', 'ejs');
+
+// listen for express 
+app.listen(3000);
+
+// render a view
+app.get('/', (req, res) => {
+    const blogs = [
+        {title: 'Yoshi11 finds eggs', snippet: 'Lorem ipsum dolor sit amet consectetur'},
+        {title: 'Yoshi22 finds eggs', snippet: 'Lorem ipsum dolor sit amet consectetur'},
+        {title: 'Yoshi33 finds eggs', snippet: 'Lorem ipsum dolor sit amet consectetur'}
+    ]
+    res.render('index', {title: 'Dynamic title', blogs});
+});
+
+app.get('/about', (req, res) => {
+    res.render('about');
+});
+
+app.get('/blogs/create', (req, res) => {
+    res.render('create');
+});
+
+// 404 
+app.use((req, res) => {
+    res.status(404).render('404');
+});
